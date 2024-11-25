9fbb878b226cb7b0eedad26418ed753f9796bf8e
Hrithik kumar <hritikkumar09sgnv.grd@gmail.com>
2024-11-25 18:14:15 +0530

    done

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Client, Users } from 'node-appwrite';
 
 // This Appwrite function will be executed every time your function is triggered
@@ -32,4 +33,37 @@ export default async ({ req, res, log, error }) => {
     connect: "https://appwrite.io/discord",
     getInspired: "https://builtwith.appwrite.io",
   });
+=======
+import OpenAI from 'openai';
+import { getStaticFile, throwIfMissing } from './utils.js';
+
+export default async ({ req, res }) => {
+  throwIfMissing(process.env, ['OPENAI_API_KEY']);
+
+  if (req.method === 'GET') {
+    return res.text(getStaticFile('index.html'), 200, {
+      'Content-Type': 'text/html; charset=utf-8',
+    });
+  }
+
+  try {
+    throwIfMissing(req.body, ['prompt']);
+  } catch (err) {
+    return res.json({ ok: false, error: err.message }, 400);
+  }
+
+  const openai = new OpenAI();
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4o',
+      max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS ?? '512'),
+      messages: [{ role: 'user', content: req.body.prompt }],
+    });
+    const completion = response.choices[0].message.content;
+    return res.json({ ok: true, completion }, 200);
+  } catch (err) {
+    return res.json({ ok: false, error: 'Failed to query model.' }, 500);
+  }
+>>>>>>> e8be25b (commit done)
 };
