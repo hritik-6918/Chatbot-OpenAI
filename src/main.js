4774d05aa7b43c6eb318ef3d998099c82d97b434
Hrithik kumar <hritikkumar09sgnv.grd@gmail.com>
2024-11-25 18:16:37 +0530

    commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Client, Users } from 'node-appwrite';
 
 // This Appwrite function will be executed every time your function is triggered
@@ -16,54 +15,21 @@ export default async ({ req, res, log, error }) => {
     // Log messages and errors to the Appwrite Console
     // These logs won't be seen by your end users
     log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
+  } catch (err) {
+    error('Could not list users: ' + err.message);
   }
 
   // The req object contains the request data
-  if (req.path === "/ping") {
+  if (req.path === '/ping') {
     // Use res object to respond with text(), json(), or binary()
     // Don't forget to return a response!
-    return res.text("Pong");
+    return res.text('Pong');
   }
 
   return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
+    motto: 'Build like a team of hundreds_',
+    learn: 'https://appwrite.io/docs',
+    connect: 'https://appwrite.io/discord',
+    getInspired: 'https://builtwith.appwrite.io',
   });
-=======
-import OpenAI from 'openai';
-import { getStaticFile, throwIfMissing } from './utils.js';
-
-export default async ({ req, res }) => {
-  throwIfMissing(process.env, ['OPENAI_API_KEY']);
-
-  if (req.method === 'GET') {
-    return res.text(getStaticFile('index.html'), 200, {
-      'Content-Type': 'text/html; charset=utf-8',
-    });
-  }
-
-  try {
-    throwIfMissing(req.body, ['prompt']);
-  } catch (err) {
-    return res.json({ ok: false, error: err.message }, 400);
-  }
-
-  const openai = new OpenAI();
-
-  try {
-    const response = await openai.chat.completions.create({
-      model: 'gpt-4o',
-      max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS ?? '512'),
-      messages: [{ role: 'user', content: req.body.prompt }],
-    });
-    const completion = response.choices[0].message.content;
-    return res.json({ ok: true, completion }, 200);
-  } catch (err) {
-    return res.json({ ok: false, error: 'Failed to query model.' }, 500);
-  }
->>>>>>> e8be25b (commit done)
 };
